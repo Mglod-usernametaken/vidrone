a117248f7ce7b5a50a6eb8b16ff3ec52589815b9
Mglod-usernametaken <mastrwloczykij@gmail.com>
2024-02-24 13:17:41 +0100

    add fulscreen capability

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-
 import cv2
 import time
 
 def show_and_record_video(device=0, output_file="output.avi"):
     cap = cv2.VideoCapture(device)
-    cap.set(cv2.CAP_PROP_FPS, 60)
+    cap.set(cv2.CAP_PROP_FPS, 30)
     cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
     if not cap.isOpened():
         print("error - could not open device")
@@ -21,16 +20,13 @@ def show_and_record_video(device=0, output_file="output.avi"):
                 print("error - could not read frame")
                 break
             fps = cap.get(cv2.CAP_PROP_FPS)
-            # print(f"fps: {fps:.2f}")
-            cv2.putText(frame,f'fps: {fps:.2f}',(50, 50),cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 255, 255),2,cv2.LINE_4) 
-            # Write the frame to the output file
-            frame = cv2.resize(frame, (960, 720), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
+            cv2.putText(frame, f'fps: {fps:.2f}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_4) 
+            frame = cv2.resize(frame, (960, 720), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
 
-
+            cv2.namedWindow('vid', cv2.WINDOW_NORMAL)
+            cv2.setWindowProperty('vid', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         
-           # out.write(frame)
-            
-            cv2.imshow('Video', frame)
+            cv2.imshow('vid', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             
